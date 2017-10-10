fbc37929710d4324588711a265152f340f90ad13
Dillon Eng <dillon@MacBook-Pro.local>
2017-10-09 22:43:20 -0700

    add port discovery for heroku

--- src/main/com/kempo/easyride/Server.java
+++ src/main/com/kempo/easyride/Server.java
@@ -4,8 +4,16 @@ import static spark.Spark.*;
 public class Server {
     public static void main(String[] args)
     {
+        getHerokuAssignedPort();
         staticFileLocation("/public");
         get("/ping", (req, res) -> "pong");
+    }
 
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
