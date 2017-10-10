c5eb42a8950fc42dc926d9ce66bd0c767fe214e2
Dillon Eng <dillon@MacBook-Pro.local>
2017-10-09 23:59:05 -0700

    heroku commit to get ping working. public dir still does not work

--- src/main/java/com/kempo/easyride/Server.java
+++ src/main/java/com/kempo/easyride/Server.java
@@ -4,7 +4,7 @@ import static spark.Spark.*;
 public class Server {
     public static void main(String[] args)
     {
-        getHerokuAssignedPort();
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         get("/ping", (req, res) -> "pong");
     }
