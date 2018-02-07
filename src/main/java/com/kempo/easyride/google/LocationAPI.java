7a4ce0c9794bac05acbd856b61091420df1f645d
kempo <ilestkempo@gmail.com>
2018-02-07 13:14:23 -0800

    added correct JSON parsing capabilities

--- src/main/java/com/kempo/easyride/google/LocationAPI.java
+++ src/main/java/com/kempo/easyride/google/LocationAPI.java
@@ -1,15 +1,21 @@
 package com.kempo.easyride.google;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
 public class LocationAPI {
-    private static final String API_KEY = System.getenv("LOCATION_KEY");
+    private static final String LOCATION_KEY = System.getenv("LOCATION_KEY");
 
     private static final String DELIMITER = "%20";
 
+    /** deprecated. this is a very wrong way of handling JSON parsing
+     *
     public static boolean isLocationValid(String address) {
+
         final String formatted = address.replaceAll(" ", DELIMITER);
         final String link = "https://maps.googleapis.com/maps/api/geocode/json?address=" + formatted + "&key=" + API_KEY;
         try {
@@ -20,10 +26,46 @@ public class LocationAPI {
                 if(line.contains("\"status\" : \"ZERO_RESULTS\"")) {
                     return false;
                 }
+
             }
         }catch(Exception e) {
             e.printStackTrace();
         }
         return true;
     }
+    **/
+
+    public static String getFormattedAddress(String address) {
+
+        final String formatted = address.replaceAll(" ", DELIMITER);
+        final String link = "https://maps.googleapis.com/maps/api/geocode/json?address=" + formatted + "&key=" + LOCATION_KEY;
+        try {
+            URL url = new URL(link);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            JSONObject obj = new JSONObject(response.toString());
+            JSONArray results = obj.getJSONArray("results");
+            if(results.isNull(0)) { // if the results are not valid
+                return null;
+            }else {
+                if (!results.getJSONObject(0).isNull("formatted_address")) { // checks if 'formatted_address' is null
+                    String a = results.getJSONObject(0).get("formatted_address").toString(); // stores data
+                    return a;
+                }
+            }
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
 }
--- src/main/java/com/kempo/easyride/google/MapsAPI.java
+++ src/main/java/com/kempo/easyride/google/MapsAPI.java
@@ -1,4 +1,7 @@
 package com.kempo.easyride.google;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -7,17 +10,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MapsAPI {
-    private final String KEY = System.getenv("MAPS_KEY"); // Google Distance Matrix API key
+    private static final String MAPS_KEY = System.getenv("MAPS_KEY"); // Google Distance Matrix API key
 
-    private final String DELIMITER = "%20";
+    private static final String DELIMITER = "%20";
 
 
     /**
-     *
+     * deprecated. very poor way of handling JSON parsing once again
      * @param o
      * @param d
      * @return distance in kilometers
-     */
+
     public double getDistance(String o, String d) {
         String link = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + setOrigin(o) + "&destinations=" + setDestination(d) + "&key=" + KEY;
         String distance = "";
@@ -28,6 +31,7 @@ public class MapsAPI {
 
             String line;
             while((line = reader.readLine()) != null) {
+                System.out.println(line);
                 if(line.contains("\"distance\"")) { // if the current line contains "distance"; this is where we parse the lines on Google's response
                     distance = reader.readLine(); // sets our variable to the next line (where the distance in kilometers will be displayed)
                 }
@@ -36,39 +40,69 @@ public class MapsAPI {
             e.printStackTrace();
         }
 
-
-        if(!distance.isEmpty() && !distance.contains("[]")) { // if we have found our line containing the distance
-            return parseDouble(distance); // returns the value using our parseDouble method
+        if(distance.isEmpty() || distance.contains("[]")) { // if our string is empty or if it contains [] then it's invalid and should'nt be parsed
+            System.out.println("invalid distance string= '" + distance + "'");
+            return -1;
         }
 
-        return -1; // if no distance value is found
+        double dist = parseDouble(distance); // parses the distance string
+
+        if(dist < 0) { // if it is less than 0, meaning an unsuccessful and invalid parse
+            System.out.println("unsuccessful parse");
+        }
+
+        return dist;
     }
+    */
 
     /**
-     *
-     * @param s
-     * @return a specific double (our distance) from a string
-     * credits to 'Bohemian'
+     * @param o
+     * @param d
+     * @return distance in kilometers
      */
-    public double parseDouble(String s) {
-        s = s.replaceAll("\"","");
-        Scanner st = new Scanner(s);
-        while (!st.hasNextDouble())
-        {
-            st.next();
+    public static double getDistance(String o, String d) {
+        String link = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + format(o) + "&destinations=" + format(d) + "&key=" + MAPS_KEY;
+        try {
+            URL url = new URL(link);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream())); // creates a BufferedReader that can read our URL
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+            JSONObject obj = new JSONObject(response.toString());
+            JSONArray rows = obj.getJSONArray("rows");
+            JSONObject element = rows.getJSONObject(0);
+            String distString = element.getJSONArray("elements").getJSONObject(0).getJSONObject("distance").get("text").toString();
+            double distance = parseDouble(distString);
+            return distance;
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        double value = st.nextDouble();
-        return value;
-        //return -1; // if no double is found
+
+        return -1;
+    }
+
+    private static double parseDouble(String s) {
+        s = s.replaceAll(",", "");
+        if(s.contains("mi")) {
+            s = s.replace("mi", "");
+            Double d = Double.parseDouble(s);
+            return d; // default measuring unit is miles
+        }else if (s.contains("ft")){
+            s = s.replace("ft", "");
+            Double d = Double.parseDouble(s);
+            return (d / 5280); // if the calculations are in feet, we want to return it in form of miles
+        }
+        return -1;
     }
 
 
-    private String setOrigin(String s) {
+    private static String format(String s) {
         return s.replaceAll(" ",DELIMITER);
-    }
-
-    private String setDestination(String s) {
-        return s.replaceAll(" ", DELIMITER);
     }
 
 }
