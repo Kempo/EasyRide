249dbe9a5612e8bff7df9064a02ed61bf05f7aaf
kempo <ilestkempo@gmail.com>
2018-02-08 11:49:01 -0800

    added DEFAULT_STATE analysis to the LocationAPI so addresses given can be placed into their appropriate state, which will by default be Washington.

--- src/main/java/com/kempo/easyride/google/LocationAPI.java
+++ src/main/java/com/kempo/easyride/google/LocationAPI.java
@@ -3,41 +3,33 @@ package com.kempo.easyride.google;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import javax.xml.bind.annotation.XmlType;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LocationAPI {
     private static final String LOCATION_KEY = System.getenv("LOCATION_KEY");
 
     private static final String DELIMITER = "%20";
 
-    /** deprecated. this is a very wrong way of handling JSON parsing
-     *
-    public static boolean isLocationValid(String address) {
+    private static final String[] DEFAULT_STATE = {"WASHINGTON", "washington", "wa", "WA"};
 
-        final String formatted = address.replaceAll(" ", DELIMITER);
-        final String link = "https://maps.googleapis.com/maps/api/geocode/json?address=" + formatted + "&key=" + API_KEY;
-        try {
-            URL url = new URL(link);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line;
-            while((line = reader.readLine()) != null) {
-                if(line.contains("\"status\" : \"ZERO_RESULTS\"")) {
-                    return false;
-                }
+    private static final ArrayList<String> STATE_LIST = new ArrayList<>();
 
-            }
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
-        return true;
+    static {
+        STATE_LIST.addAll(Arrays.asList(DEFAULT_STATE));
     }
-    **/
 
-    public static String getFormattedAddress(String address) {
+    public static String getFormattedAddress(String unformatted) {
+        StringBuilder address = new StringBuilder(unformatted);
+        if(!isStateDeclared(unformatted)) {
+            address.append(" " + DEFAULT_STATE[0]);
+        }
+        final String formatted = address.toString().replaceAll(" ", DELIMITER);
 
-        final String formatted = address.replaceAll(" ", DELIMITER);
         final String link = "https://maps.googleapis.com/maps/api/geocode/json?address=" + formatted + "&key=" + LOCATION_KEY;
         try {
             URL url = new URL(link);
@@ -66,6 +58,16 @@ public class LocationAPI {
         return null;
     }
 
+    private static boolean isStateDeclared(String address) {
+        for(String state : STATE_LIST) {
+            if (address.contains(state)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-
+    public static void addDefaultStates(String[] abbreviations) {
+        STATE_LIST.addAll(Arrays.asList(abbreviations));
+    }
 }
