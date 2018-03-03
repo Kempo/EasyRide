6146bd19c59398c99bdaebad2fbac96d13885247
kempo <ilestkempo@gmail.com>
2018-03-02 21:44:49 -0800

    added an UnitTest integrating the Google Sheets API so I don't have to always deploy my project to identify bugs. This makes things a whole lot easier! I also should've done this earlier.

--- src/test/java/com/kempo/easyride/util/OrchestratorTest.java
+++ src/test/java/com/kempo/easyride/util/OrchestratorTest.java
@@ -1,7 +1,10 @@
 package com.kempo.easyride.util;
 
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import com.kempo.easyride.application.Orchestrator;
 import com.kempo.easyride.application.RideAssigner;
+import com.kempo.easyride.google.SheetsAPI;
 import com.kempo.easyride.model.AssignedRides;
 import com.kempo.easyride.model.Driver;
 import com.kempo.easyride.model.RawParticipants;
@@ -14,8 +17,24 @@ public class OrchestratorTest extends TestCase {
     private Orchestrator orchestrator = new Orchestrator(new RideAssigner());
 
 
-    public void testWithSheetsData() {
+    public void testWithSheetsData() throws Exception {
+        String dataRange = "A2:D16";
+        String sheetsID = SheetsAPI.getIDFromURL("https://docs.google.com/spreadsheets/d/1wQPfybTVK-d_2F4ArVD6I_IRK7iNgY1L1rDm5PM-HVw/edit?ouid=101645746693707733801&usp=sheets_home&ths=true");
+        Sheets service = SheetsAPI.getSheetsService();
+        ValueRange values = service.spreadsheets().values().get(sheetsID, dataRange).setKey(SheetsAPI.API_KEY).execute();
 
+        final RawParticipants participants = parser.parseInitialRequestThroughSheets(values);
+        final AssignedRides result = orchestrator.orchestrateRides(participants);
+
+        for(Driver d : result.getDrivers()) {
+            System.out.println("NAME: " + d.getName());
+            for(Rider r : d.getCar().getOccupants()) {
+                System.out.println(r.getName());
+            }
+            System.out.println();
+        }
+
+        System.out.println(result.getUnassignedOrUnparseable());
     }
 
     public void testWithTextInputAndDriversWithLimitedSpots() {
