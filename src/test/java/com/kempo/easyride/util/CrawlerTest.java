9eab0ca873ac04dd28a77c931aba834361308d0b
kempo <ilestkempo@gmail.com>
2018-04-18 22:45:54 -0700

    crawler test fix

--- src/test/java/com/kempo/easyride/util/CrawlerTest.java
+++ src/test/java/com/kempo/easyride/util/CrawlerTest.java
@@ -1,11 +1,11 @@
 package com.kempo.easyride.util;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
 
 public class CrawlerTest extends TestCase {
 
-    public void crawlThroughSheetsBasic() {
+    public void testCrawler() {
         System.out.println("test!");
     }
-
 }
