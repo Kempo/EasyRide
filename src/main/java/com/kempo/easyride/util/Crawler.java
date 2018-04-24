9424b2502a09d0ee6f74450ee0452a1c5c66bd7e
kempo <ilestkempo@gmail.com>
2018-04-24 12:24:06 -0700

    reworked crawler algorithm. will continue to revise!! many bugs to fix.

--- src/main/java/com/kempo/easyride/util/Crawler.java
+++ src/main/java/com/kempo/easyride/util/Crawler.java
@@ -18,6 +18,7 @@ public class Crawler {
 
     private int nameCol = -1, addressCol = -1, designationCol = -1, spotsCol = -1; // default = -1
     private int startingRow = -1; // starting row that contains all the column sets
+
     /**
      * on initialization, Crawler will store the values given and then utilise them for parsing.
      * I could honestly just pass everything through parameters though, but that might be a little messy.
@@ -31,11 +32,6 @@ public class Crawler {
         this.values = generateValues();
         crawl();
     }
-
-    private final List getList(String[] obj) {
-        return Arrays.asList(obj);
-    }
-
     public int getNameColumn() { return nameCol; }
     public int getAddressColumn() { return addressCol; }
     public int getDesignationColumn() { return designationCol; }
@@ -48,44 +44,53 @@ public class Crawler {
      * Intended for a Google form output into a user's sheets file.
      */
     private final void crawl() {
-        if(values != null) {
+        if (values != null) { // if our current sheets data set is available
             final List<List<Object>> rows = values.getValues();
-            for(List currentRow : rows) { // loops through all the rows
-                if(currentRow.size() >= minimumRowLength) { // if the row size (# of columns) fits criteria for identifying columns
-                    for(int colIndex = 0; colIndex < currentRow.size(); colIndex++) { // loops through all the columns in that row
+            mainLoop : for (List currentRow : rows) { // loops through all the rows as a block; this will stop looping once all data columns are identified
+                if (currentRow.size() >= minimumRowLength) { // if the row size (# of columns) fits criteria for identifying columns
+                    for (int colIndex = 0; colIndex < currentRow.size(); colIndex++) { // loops through all the columns in that row
                         String colValue = currentRow.get(colIndex).toString(); // gets the value of each column
-                        if(colValue != null && colValue.length() > 2) { // if the value in each column isn't null or empty
-                            mainloop: // main loop block
-                            {
-                                for (String[] items : Keywords.requirements) {
-                                    for (String k : items) {
-                                        if (colValue.toLowerCase().contains(k)) {
-                                            // set columns for designation
-                                            if (isStringInList(colValue, Keywords.ADDRESSES)) {
-                                                addressCol = colIndex;
-                                            }
+                        if (colValue != null && !colValue.isEmpty()) { // if the value in each column is a question and isn't null or empty
+                            for (String[] items : Keywords.requirements) {
+                                for (String k : items) {
+                                    if (colValue.toLowerCase().contains(k)) { // if the column value contains any of the strings in the two-dimensional array requirements
+                                        // set columns for designation if the data columns have not been set already.
+                                        if (isStringInList(colValue, Keywords.ADDRESSES) && addressCol == -1) {
+                                            addressCol = colIndex;
+                                        }
 
-                                            if (isStringInList(colValue, Keywords.NAMES)) {
-                                                nameCol = colIndex;
-                                            }
+                                        if (isStringInList(colValue, Keywords.NAMES) && nameCol == -1) {
+                                            nameCol = colIndex;
+                                        }
 
-                                            if (isStringInList(colValue, Keywords.SIZE)) {
+                                        if(colValue.contains("?")) {
+                                            if(spotsCol == -1 && designationCol != -1) {
                                                 spotsCol = colIndex;
-                                            }
-
-                                            for (String[] i : Keywords.DESIGNATIONS) {
-                                                for (String s : i) {
-                                                    if (colValue.contains(s)) {
-                                                        designationCol = colIndex;
-                                                        break; // breaks out of current loop
-                                                    }
+                                            }else if(spotsCol != -1 && designationCol == -1) {
+                                                designationCol = colIndex;
+                                            }else if(spotsCol == -1 && designationCol == -1) {
+                                                boolean decider = getSimilarity(colValue, Keywords.SIZE) > getSimilarity(colValue, Keywords.DQUESTION);
+                                                if(decider) {
+                                                    spotsCol = colIndex;
+                                                }else if (decider == false){
+                                                    designationCol = colIndex;
                                                 }
                                             }
+                                        }
 
-                                            if (nameCol != -1 && addressCol != -1 && designationCol != -1 && spotsCol != -1) { // if all columns needed are identified
-                                                System.out.println("All columns identified.");
-                                                break mainloop; // breaks out of the mainloop block
-                                            }
+                                        /**
+                                        if (colValue.contains("?") && isStringInList(colValue, Keywords.SIZE) && spotsCol == -1) {
+                                            spotsCol = colIndex;
+                                        }
+
+                                        if(colValue.contains("?") && isStringInList(colValue, Keywords.DQUESTION) && designationCol == -1) {
+                                            designationCol = colIndex;
+                                        }
+                                        **/
+
+                                        if (nameCol != -1 && addressCol != -1 && designationCol != -1 && spotsCol != -1) { // if all columns needed are identified
+                                            System.out.println("All columns identified.");
+                                            break mainLoop; // breaks out of the mainloop block
                                         }
                                     }
                                 }
@@ -103,7 +108,7 @@ public class Crawler {
      * @param items
      * @return true if a string has components that is in a list and false if it doesn't. credits to StackOverFlow java-ers
      */
-    private boolean isStringInList(String inputStr, String[] items) {
+    public boolean isStringInList(String inputStr, String[] items) {
         return Arrays.stream(items).parallel().anyMatch(inputStr::contains);
     }
 
@@ -112,6 +117,16 @@ public class Crawler {
      * @throws IOException
      */
     private final ValueRange generateValues() throws IOException {
-        return service.spreadsheets().values().get(sheetsID, "A:Z").setKey(SheetsAPI.API_KEY).execute();
+        return (sheetsID != null || !sheetsID.isEmpty()) ? ((service.spreadsheets().values().get(sheetsID, "A:Z").setKey(SheetsAPI.API_KEY).execute())) : null;
+    }
+
+    /**
+     *
+     * @param element
+     * @param array
+     * @return the number of components a string has that is in a string array
+     */
+    private final double getSimilarity(String element, String[] array) {
+        return Arrays.stream(array).parallel().filter(element::contains).count();
     }
 }
--- src/main/java/com/kempo/easyride/util/Keywords.java
+++ src/main/java/com/kempo/easyride/util/Keywords.java
@@ -7,14 +7,30 @@ public class Keywords {
     public static final String[]
             NAMES = {"name"},
             ADDRESSES = {"address", "live"},
-            SIZE = {"spots", "size", "car"};
+            SIZE = {"spots", "how many", "car", "you have"}; // spots question keywords
 
-    private static final String[]
-            RIDERYES = {""},
-            RIDERNO = {""},
-            DRIVER = {""};
+    public static final String[] // actual responses keywords
+            RIDER = {"need", "yes"},
+            DRIVER = {"can", "no"};
 
-    public static final String[][] DESIGNATIONS = {RIDERYES, RIDERNO, DRIVER};
+    public static final String[] // designation question keywords
+            DQUESTION = {"will you","how are you", "do you"};
 
-    public static final String[][] requirements = {NAMES, ADDRESSES, SIZE, RIDERYES, RIDERNO, DRIVER};
+    public static final String[][] requirements = {NAMES, ADDRESSES, SIZE, DQUESTION};
 }
+
+
+/**
+ * I don't need a ride and I cannot give a ride (null)
+ * I don't need a ride and I can give a ride (DRIVER)
+ * I don't need a ride (null)
+ * I need a ride (RIDER)
+ *
+ * I cannot give a ride. (null)
+ * I can give a ride. (DRIVER)
+ *
+ * How are you getting to _____?
+ * Will you need a ride?
+ * Do you need a ride?
+ *
+ **/
