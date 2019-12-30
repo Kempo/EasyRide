933878bfb2265db9734d07f3b6c7934d6172aba7
Aaron <aaronc@timpli.com>
2019-12-29 18:09:52 -0800

    enabled CORS - only for testing

--- src/main/java/com/kempo/easyride/Server.java
+++ src/main/java/com/kempo/easyride/Server.java
@@ -8,10 +8,7 @@ import com.kempo.easyride.util.ServerHelper;
 
 import java.nio.charset.StandardCharsets;
 
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class Server {
 
@@ -22,6 +19,7 @@ public class Server {
     {
         port(getHerokuAssignedPort());
         staticFileLocation("/public");
+        enableCORS("*", "*", "*"); // only to be used for testing cases!
         get("/ping", (req, res) -> {
             System.out.println("hello!");
             return "pong";
@@ -46,6 +44,33 @@ public class Server {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    // Enables CORS on requests. This method is an initialization method and should be called once.
+    private static void enableCORS(final String origin, final String methods, final String headers) {
+
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", origin);
+            response.header("Access-Control-Request-Method", methods);
+            response.header("Access-Control-Allow-Headers", headers);
+            // Note: this may or may not be necessary in your particular application
+            response.type("application/json");
+        });
     }
 
 }
